e91a9c8ab2543f5959af384fb8968552c16ac533
ITC\jesus.serrato <jesus.serrato@getromics.com>
2020-09-16 18:47:29 +0200

    no message

---
+++ practicaCurso/src/app/pages/resources/resources-detail/resources-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resources-detail',
+  templateUrl: './resources-detail.component.html',
+  styleUrls: ['./resources-detail.component.css']
+})
+export class ResourcesDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
