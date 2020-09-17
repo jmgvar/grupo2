d7177abc47398925fd6def500a1f3746c03a2de8
GETRONICS\jgomez3 <juan-manuel.gomez@getronics.com>
2020-09-17 17:55:26 +0200

    no message

--- practicaCurso/src/app/pages/auth/login/login.component.ts
+++ practicaCurso/src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { User } from '../../../models/user';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,22 @@ export class LoginComponent implements OnInit {
   u: User;
   title = 'Hola';
 
-  constructor() { 
+  constructor(private http: HttpClient) { 
     this.u = new User();
   }
 
   ngOnInit(): void {
   }
 
+  login() {
+    console.log(this.u.email);
+    console.log(this.u.password);
+    this.http.post("https://reqres.in/api/login", this.u).subscribe( data => {
+      console.log(data);
+    });
+  }
+
+  register() {
+    
+  }
 }
